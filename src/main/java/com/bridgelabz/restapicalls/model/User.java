e473167f5a68d044ef7ba75f4e1f2613d67d4c16
shindeanant <shindeanant1997@gmail.com>
2021-12-13 20:56:29 +0530

    Added:Rest Call to displaying message by Using Put Method.

--- src/main/java/com/bridgelabz/restapicalls/model/User.java
+++ src/main/java/com/bridgelabz/restapicalls/model/User.java
@@ -1,20 +1,35 @@
 package com.bridgelabz.restapicalls.model;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.PutMapping;
+
 public class User {
     private String firstName;
     private String lastName;
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-   
+
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, 
+                            @RequestParam(value = "lastName") String lastName) {
+        return "Hello " + firstName + " " + lastName + " From bridgeLabz";
+    }
 
 }
