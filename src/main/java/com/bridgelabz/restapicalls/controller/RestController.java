902f4a4595a1d7d0824cf9521f58e283e13ccf92
shindeanant <shindeanant1997@gmail.com>
2021-12-13 20:26:20 +0530

    Added: REST Call Using GET Request Method Taking Path Variable.

--- src/main/java/com/bridgelabz/restapicalls/controller/RestController.java
+++ src/main/java/com/bridgelabz/restapicalls/controller/RestController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.restapicalls.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +18,14 @@ public class RestController {
     public String sayHello(@RequestParam(value="name") String name) {
         return "Hello" + name + "From Bridgelabz";
     }
-    
+    @RequestMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + " From Bridgelabz";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloPara(@PathVariable String name) {
+        return "Hello " + name + " From Bridgelabz";
+    }
     
 }
